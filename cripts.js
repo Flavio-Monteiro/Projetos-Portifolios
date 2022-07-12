508afd46732676d233de69f6672f29a6003b4f20
Flávio Monteiro <flavio.ms@outlook.com.br>
2022-07-12 07:56:27 -0300

    novos js adcionados

---
+++ cripts.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({ reset: true });
+
+
+ScrollReveal().reveal('.flavio', { duration: 1000 })
+ScrollReveal().reveal('.monteiro', { delay: 900 })
+
+sr.reveal('#project',{duration: 1500})
+sr.reveal('#skills',{duration: 1500})
+
+
+ScrollReveal().reveal('.img-skills', { interval: 200 });
+
