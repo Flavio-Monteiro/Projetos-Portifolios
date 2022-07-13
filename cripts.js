add27f34cbd2ee761c7f1c33d7ead81d706d601d
Flávio Monteiro <flavio.ms@outlook.com.br>
2022-07-13 19:33:30 -0300

    novo backgrounds  add

--- cripts.js
+++ cripts.js
@@ -2,7 +2,7 @@ window.sr = ScrollReveal({ reset: true });
 
 
 ScrollReveal().reveal('.flavio', { duration: 1000 })
-ScrollReveal().reveal('.monteiro', { duratin: 900 })
+ScrollReveal().reveal('.monteiro', { duration: 900 })
 
 sr.reveal('#project',{duration: 1500})
 sr.reveal('#skills',{duration: 1500})
@@ -12,5 +12,6 @@ ScrollReveal().reveal('.img-skills', { interval: 200 });
 
 ScrollReveal().reveal('.cards-skills', { interval: 200 });
 
+ScrollReveal().reveal('.container', {duration :3000})
 
 
